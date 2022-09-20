a6eebc315cf394a2aed6979243103692fcf17294
Yasser El-Tantawi <yasserbadi4@gmail.com>
2022-09-20 15:56:59 +0200

    start working on yasser's booking feature

--- lib/src/core/api/endpoints.dart
+++ lib/src/core/api/endpoints.dart
@@ -2,4 +2,5 @@ class Endpoints {
   static const String baseUrl = 'http://api.mahmoudtaha.com/api';
   static const String auth = '$baseUrl/auth';
   static const String login = '$auth/login';
+  static const String getBooking = '$baseUrl/get-bookings';
 }
