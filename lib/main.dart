da2da94d391ed4c6f644c8607e1f53ad1bca098f
Yasser El-Tantawi <yasserbadi4@gmail.com>
2022-09-22 16:01:57 +0200

    Google Maps Edits for AndroidManifest.xml and pubspec.yaml and a test Screen for using maps to be continued ISA ...

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:bookingapp/injection_container.dart' as di;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'maps_test.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   Bloc.observer = AppBlocObserver(); // runZoned is deprecated
-  runApp(const BookingApp());
+  runApp(const MyApp());
 }
